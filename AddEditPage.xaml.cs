2903b52c91e8c8542b0192701f27ed1ca18a4a36
Элина Хамитова <khamitovaelina09@gmail.com>
2023-10-30 22:21:43 +0300

    add function closed. textbox style changed (placeholder - watermark added).

--- AddEditPage.xaml.cs
+++ AddEditPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +21,41 @@ namespace NedvizhPr
     /// </summary>
     public partial class AddEditPage : Page
     {
+        private Client _curClient = new Client();
         public AddEditPage()
         {
             InitializeComponent();
+            DataContext = _curClient;
         }
 
         private void SaveBtn_Click(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            StringBuilder err = new StringBuilder();
+            var lastid = nedvizhdbEntities.GetContext().Clients.ToList().Last().Id;
+            int nid = lastid + 1;
+            _curClient.Id = nid;
+            
+            if (string.IsNullOrWhiteSpace(_curClient.Phone) && string.IsNullOrWhiteSpace(_curClient.Email))
+                err.AppendLine("Укажите Телефон или Email");
+
+            if (err.Length > 0)
+            {
+                MessageBox.Show(err.ToString());
+                return;
+            }
+            
+            nedvizhdbEntities.GetContext().Clients.Add(_curClient);
+
+            try
+            {
+                nedvizhdbEntities.GetContext().SaveChanges();
+                MessageBox.Show("Клиент добавлен");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
--- ClientsPage.xaml.cs
+++ ClientsPage.xaml.cs
@@ -23,7 +23,6 @@ namespace NedvizhPr
         public ClientsPage()
         {
             InitializeComponent();
-            DGridClients.ItemsSource = nedvizhdbEntities.GetContext().Clients.ToList();
         }
 
         private void EditBtn_Click(object sender, RoutedEventArgs e)
@@ -53,6 +52,15 @@ namespace NedvizhPr
                 {
                     MessageBox.Show(ex.Message.ToString());
                 }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                nedvizhdbEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridClients.ItemsSource = nedvizhdbEntities.GetContext().Clients.ToList();
             }
         }
     }
