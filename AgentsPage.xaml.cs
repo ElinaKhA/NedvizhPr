9b50c1957b44dcce546bb767911072af8840e8f7
Элина Хамитова <khamitovaelina09@gmail.com>
2023-10-31 15:49:33 +0300

    Work with agents created. Functions is not working

--- AgentsPage.xaml.cs
+++ AgentsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DuoVia.FuzzyStrings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,92 @@ namespace NedvizhPr
         public AgentsPage()
         {
             InitializeComponent();
+        }
+        public class AgentData
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string MiddleName { get; set; }
+            public string LastName { get; set; }
+            public string DealShare { get; set; }
+            public int Demand { get; set; }
+            public int Supply { get; set; }
+        }
+
+
+        public List<AgentData> GetAllAgentsData(nedvizhdbEntities nedvizhdbEntities)
+        {
+            var agents = nedvizhdbEntities.Agents.ToList();
+            var demands = nedvizhdbEntities.Demands.ToList();
+            var supplies = nedvizhdbEntities.Supplies.ToList();
+
+            var allAgents = agents.Select(ag => new AgentData
+            {
+                Id = ag.Id,
+                FirstName = ag.FirstName,
+                MiddleName = ag.MiddleName,
+                LastName = ag.LastName,
+                DealShare = ag.DealShare.ToString(),
+                Demand = demands
+                    .Where(d => d.AgentId == ag.Id)
+                    .Select(d => d.Id)
+                    .FirstOrDefault(),
+                Supply = supplies
+                    .Where(s => s.AgentId == ag.Id)
+                    .Select(s => s.Id)
+                    .FirstOrDefault()
+            }).ToList();
+
+            return allAgents;
+        }
+
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+           // Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Client));
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+          //  Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var AgentsForRemoving = DGridAgents.SelectedItems.Cast<Agent>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {AgentsForRemoving.Count()} элементов?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    nedvizhdbEntities.GetContext().Agents.RemoveRange(AgentsForRemoving);
+                    nedvizhdbEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+                    DGridAgents.ItemsSource = GetAllAgentsData(nedvizhdbEntities.GetContext()).ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+       
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                nedvizhdbEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridAgents.ItemsSource = GetAllAgentsData(nedvizhdbEntities.GetContext()).ToList();
+            }
+        }
+        private void findTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchText = findTb.Text;
+            var allAgents =GetAllAgentsData(nedvizhdbEntities.GetContext()).ToList();
+            var foundAgents = allAgents.Where(ag =>
+            searchText.LevenshteinDistance(ag.FirstName) <= 3 ||
+            searchText.LevenshteinDistance(ag.MiddleName) <= 3 ||
+            searchText.LevenshteinDistance(ag.LastName) <= 3).ToList();
+            DGridAgents.ItemsSource = foundAgents.ToList();
         }
     }
 }
