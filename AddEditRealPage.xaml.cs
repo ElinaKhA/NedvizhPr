19e8ea101a3b11f8063137e2dc63a6e7ba4f21b0
Элина Хамитова <khamitovaelina09@gmail.com>
2023-11-01 13:18:44 +0300

    Work with realty ended. Add, edit, delete functions added

---
+++ AddEditRealPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NedvizhPr
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditRealPage.xaml
+    /// </summary>
+    public partial class AddEditRealPage : Page
+    {
+        private Realty _curReal = new Realty();
+        int old = 0;
+        public AddEditRealPage(Realty selectedReal)
+        {
+            InitializeComponent();
+            if (selectedReal != null)
+            {
+                _curReal = selectedReal;
+                old = 1;
+            }
+            DataContext = _curReal;
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder err = new StringBuilder();
+
+            if (old == 0)
+            {
+                var lastid = nedvizhdbEntities.GetContext().Realties.ToList().Last().Id;
+                int nid = lastid + 1;
+                _curReal.Id = nid;
+                _curReal.Coordinate_latitude = 0;
+                _curReal.Coordinate_longitude = 0;
+            }
+            if (old == 1)
+            {
+                Realty real = nedvizhdbEntities.GetContext().Realties
+                    .Where(o => o.Id == _curReal.Id)
+                    .FirstOrDefault();
+                nedvizhdbEntities.GetContext().Realties.Remove(real);
+                nedvizhdbEntities.GetContext().SaveChanges();
+            }
+
+
+            nedvizhdbEntities.GetContext().Realties.Add(_curReal);
+
+            try
+            {
+                nedvizhdbEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
--- RealtyPage.xaml.cs
+++ RealtyPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DuoVia.FuzzyStrings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,52 @@ namespace NedvizhPr
         public RealtyPage()
         {
             InitializeComponent();
+            DGridRealty.ItemsSource = nedvizhdbEntities.GetContext().Realties.ToList();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditRealPage(null));
+        }
+        private void EditBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditRealPage((sender as Button).DataContext as Realty));
+        }
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var realForRemoving = DGridRealty.SelectedItems.Cast<Realty>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {realForRemoving.Count()} элементов?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    nedvizhdbEntities.GetContext().Realties.RemoveRange(realForRemoving);
+                    nedvizhdbEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+                    DGridRealty.ItemsSource = nedvizhdbEntities.GetContext().Realties.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                nedvizhdbEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridRealty.ItemsSource = nedvizhdbEntities.GetContext().Realties.ToList();
+            }
+        }
+        private void findTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string searchText = findTb.Text;
+            var allReal = nedvizhdbEntities.GetContext().Realties.ToList();
+            var foundReal = allReal.Where(ag =>
+            searchText.LevenshteinDistance(ag.Address_City) <= 3 ||
+            searchText.LevenshteinDistance(ag.Address_Street) <= 3).ToList();
+            DGridRealty.ItemsSource = foundReal.ToList();
         }
     }
 }
